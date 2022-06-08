5ac9a87c76f82ab74aa089490d82b0464dcb288d
hirokiadachi <ha618@mprg.cs.chubu.ac.jp>
2022-06-08 19:00:42 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,13 +1,1 @@
 # Docker
-This docker file made by ha618.  
-## What is new?
-This docker image can insatll open3d, and use on Jupyter notebook.  
-
-## Other install libraly
-This docker-image based on nvidia/cuda:9.0-cudnn7-devel-ubuntu16.04  
-Python version is 3.6 for default. (i.e. you can type only `python`)  
-Chainer and cupy version is ratest release.  
-Pytorch version is 0.4.1.  
-And this image can use jupyter notebook into docker conter.  
-However, to use jupyter notebook, you need to set extarnal port which can connect web site.
-
---
+++ launch-container.sh
@@ -0,0 +1,106 @@
+#!/bin/sh
+
+read -p "Type port name: " PORT
+while :
+do
+  if [ -z "$PORT" ]; then
+    read -p "Type port name: " PORT
+  else
+    break
+  fi
+done
+
+read -p "Which directories mounts to the container?: " MOUNT_DIR
+while :
+do
+  if [ -z "$MOUNT_DIR" ]; then
+    read -p "Which directories mounts to the container?: " MOUNT_DIR
+  else
+    break
+  fi
+done
+
+read -p "Type container name: " CONTAINER_NAME
+while :
+do
+  if [ -z "$CONTAINER_NAME" ]; then
+    read -p "Type container name: " CONTAINER_NAME
+  else
+    break
+  fi
+done
+
+echo \
+"# ==============================
+# Visualize Docker image
+# ==============================
+"
+docker images
+echo \
+"
+# =============================="
+
+read -p "Waht is kind of docker image?: " DOCKER_IMAGE
+while :
+do
+  if [ -z "$DOCKER_IMAGE" ]; then
+    read -p "Waht is kind of docker image?: " DOCKER_IMAGE
+  else
+    break
+  fi
+done
+
+while :
+do
+echo \
+"
+Port number    : $PORT
+Mount directory: $MOUNT_DIR
+Container name : $CONTAINER_NAME
+Docker image   : $DOCKER_IMAGE
+"
+read -p "These arguments are true? (y/n): " CONFIRM
+  
+  if [ "$CONFIRM" = "y" ] || [ "$CONFIRM" = "Y" ]; then
+    break
+  elif [ "$CONFIRM" = "n" ] || [ "$CONFIRM" = "n" ]; then
+    read -p "Which setting do you want to change? (port/mount/name/image): " FIX
+    
+    if [ "$FIX" = "port" ]; then
+      read -p "Type port name: " PORT
+    elif [ "$FIX" = "mount" ]; then
+      read -p "Which directories mounts to the container?: " MOUNT_DIR
+    elif [ "$FIX" = "name" ]; then
+      read -p "Type container name: " CONTAINER_NAME
+    elif [ "$FIX" = "image" ]; then
+      read -p "Waht is kind of docker image?: " DOCKER_IMAGE
+    else
+      read -p "Which setting do you want to change? (port/mount/name/image): " FIX
+    fi
+    
+  else
+    read -p "These arguments are true? (y/n): " CONFIRM
+  fi
+done
+
+
+while :
+do
+  read -p "Launch jupyter lab at the same time as building the container? (y/n): " JUPYTER
+  if [ "$JUPYTER" = "y" ] || [ "$JUPYTER" = "Y" ] || [ "$JUPYTER" = "n" ] || [ "$JUPYTER" = "n" ]; then
+    break
+  fi
+done
+
+if [ "$JUPYTER" = "y" ] || [ "$JUPYTER" = "Y" ]; then
+    echo "Build docker container with $DOCKER_IMAGE and launch jupyter lab in the container"
+    docker run --runtime=nvidia --rm -it -u 1001:1001 -p $PORT:8888 \
+        -v $MOUNT_DIR:/home/workspace --ipc=host --name $CONTAINER_NAME \
+        $DOCKER_IMAGE\
+        jupyter lab --no-browser --ip=0.0.0.0 --allow-root --NotebookApp.token= --notebook-dir='/home'
+else
+    echo "Build docker container with $DOCKER_IMAGE"
+    docker run --runtime=nvidia --rm -it -u 1001:1001 -p $PORT:8888 \
+        -v $MOUNT_DIR:/home/workspace --ipc=host --name $CONTAINER_NAME \
+        $DOCKER_IMAGE
+fi
